6562b480b81957d6a7ab1b753da1ea3b62a72305
NMunro <neilmunro@gmail.com>
2025-01-06 22:11:07 +0000

    Implemented experimental ningle handle-request method request
    It is still limited to hunchentoot right now though

--- src/cl-forms.lisp
+++ src/cl-forms.lisp
@@ -627,6 +627,20 @@ Use RENDER-FIELD, RENDER-FIELD-LABEL, etc manually, after."
           do (field-read-from-request (cdr field) form
                                       post-parameters))))
 
+(defun handle-request (&optional (form *form*) (request ningle:*request*))
+  (when (form-csrf-protection-p form)
+    ;; Check the csrf token
+    (let ((session-token (get-form-session-csrf-token form)))
+      (when (or (not session-token) (not (equalp session-token (cdr (assoc (form-csrf-field-name form) (lack/request:request-body-parameters request) :test #'string=)))))
+        ;; The form is not valid. Throw an error, but reset its CSRF token for next time
+        (set-form-session-csrf-token form)
+        (error "Invalid CSRF token"))))
+
+  (let ((post-parameters (lack/request:request-body-parameters request)))
+    (loop for field in (form-fields form)
+          do (field-read-from-request (cdr field) form
+                                      post-parameters))))
+
 (defgeneric field-read-from-request (field form parameters))
 
 (defmethod field-read-from-request :around (field form parameters)
